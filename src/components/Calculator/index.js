ec93822466578ebf99b64239cec073909bda8155
zhiyingweng <zhiyingweng@brandeis.edu>
2021-02-02 15:57:08 -0500

    changed to formula logic

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -12,17 +12,15 @@ class Calculator extends Component{
 	    super(props);
 
 	    this.state = {
-				savedVal: 0,
 	      currVal: 0,
+				preVal: 0,
 				currDisplay: "0",
-	      operator: "",
-				base: 0.1,
-	      isNewNumberEntered: true,
+				base: 1,
+	      isNewNumber: true,
 	      isOperatorEntered: false,
 	      isCompleted: false,
         isDecimalAdded: false,
 				displayedEquation: "",
-				actualEquation: []
 	    }
 
 	    this.onEnterNumber = this.onEnterNumber.bind(this);
@@ -34,126 +32,148 @@ class Calculator extends Component{
   	}
 
   	onEnterNumber(num){
-			if(this.state.currVal > 9999999999) return;
-			const {currVal, actualEquation, base, isDecimalAdded, displayedEquation} = this.state;
-			if(this.state.isNewNumberEntered){
-				this.setState({
-					currVal: num,
-					currDisplay: num,
-					isNewNumberEntered: false,
-					actualEquation: [...this.state.actualEquation, num],
-					displayedEquation: displayedEquation + num
+			const {currVal, base, isDecimalAdded, isCompleted, isNewNumber, displayedEquation} = this.state;
+			if(currVal > 9999999999 || base >= 10) return;
+			// if this is the beginning of an equation
+			// or a new number after an operator
+			let newBase = base;
+			if(isCompleted || isNewNumber){
+				this.setState((state) => {
+					return {
+						isCompleted: false,
+						currVal: num,
+						currDisplay: num,
+						isNewNumber: false,
+						displayedEquation: isCompleted? "" + num : displayedEquation + num
+					}
 				});
 			}else{
 				let newVal = 0;
+				// Determine whether the value has a decimal point
 				if(isDecimalAdded){
-					newVal = currVal + num * base;
-					this.setState({
-						base: base / 10
-					});
+					newVal = currVal + num / Math.pow(10, newBase);
+					newBase++;
 				}else{
 					newVal = currVal * 10 + num;
 				}
+				console.log(newVal);
 				this.setState({
 					currVal: newVal,
-					currDisplay: newVal,
-					isNewNumberEntered: false,
-					actualEquation: [...actualEquation.slice(0, (actualEquation.length - 1)), newVal],
+					base: newBase,
+					currDisplay: base === 1 ? newVal.toString() : newVal.toPrecision(newBase - 1).toString(),
+					isNewNumber: false,
 					displayedEquation: displayedEquation + num
 				})
 			}
   	}
 
   	onAddDecimalPoint(){
+			const {currDisplay, isNewNumber,displayedEquation} = this.state;
 			if(!this.state.isDecimalAdded){
 				this.setState({
 					isDecimalAdded: true,
-					displayedEquation: this.state.displayedEquation + "."
+					currDisplay: isNewNumber ? "." : currDisplay + ".",
+					displayedEquation: displayedEquation + "."
 				});
 			}
   	}
 
   	onEnterOperator(op) {
-			const {currVal, isOperatorEntered, displayedEquation} = this.state;
-			const currResult = this.updateResult();
+			const {currVal, isCompleted, displayedEquation} = this.state;
+			let actualDisplay = displayedEquation;
+			let parsed = parseInt(displayedEquation.charAt(displayedEquation.length - 1));
+			if(isNaN(parsed)){
+				actualDisplay = displayedEquation.slice(0, displayedEquation.length - 1);
+			}
 			this.setState({
-				savedVal: isOperatorEntered ? currResult: currVal,
 				currVal: 0,
-				operator: op,
-				base: 0.1,
+				base: 1,
 				isCompleted: false,
-				isNewNumberEntered: true,
+				isNewNumber: true,
 				isDecimalAdded: false,
 				isOperatorEntered: true,
-				displayedEquation: displayedEquation + op
+				displayedEquation: isCompleted ? currVal + op : actualDisplay + op
 			});
   	}
 
   	updateResult(){
-  		const {currVal, savedVal, operator} = this.state;
-			let newVal = savedVal;
-  		switch(operator){
-  			case "+":
-  				newVal += currVal;
-  				break;
-  			case "-":
-  				newVal -= currVal;
-  				break;
-  			case "*":
-  				newVal *= currVal;
-  				break;
-  			case "/":
-					if(currVal === 0){
-						return null;
+  		const {displayedEquation} = this.state;
+			let vals = [];
+			let ops = [];
+			let parts = displayedEquation.split(/(\+|-|\*|\/)/);
+			if(parts[0] === ""){
+				parts[0] = "0";
+			}
+			console.log(parts);
+			for(let s of parts){
+				if(["+", "-", "*", "/"].includes(s)){
+					ops.push(s);
+				}else{
+					let op = ops[ops.length - 1];
+					if(["*", "/"].includes(op)){
+						let val = parseFloat(s);
+						ops.pop();
+						let preVal = vals.pop();
+						if(op === "*"){
+							vals.push(preVal * val);
+						}else{
+							vals.push(preVal / val);
+						}
+					}else{
+						vals.push(parseFloat(s));
 					}
-  				newVal /= currVal;
-  				break;
-  			default:
-  				newVal = currVal;
-  		}
-  		return newVal;
+				}
+			}
+			console.log("vals:" + vals);
+			console.log("ops: " + ops);
+			vals = vals.reverse();
+			ops = ops.reverse();
+			while(ops.length !== 0){
+				let op = ops.pop();
+				let preVal = vals.pop();
+				let val = vals.pop();
+				if(op === "+"){
+					vals.push(preVal + val);
+				}else vals.push(preVal - val);
+			}
+			return vals.pop();
   	}
 
   	onEqualTo(){
 			const currResult = this.updateResult();
-			if(currResult === null){
+			if(isNaN(currResult)){
 				this.onReset();
 				this.setState({
 					currDisplay: "Error"
 				});
 				return;
 			}
-			console.log(currResult + " length is " + currResult.toString().length);
 			const resultDisplay = currResult.toString().length <= 12 ? currResult : currResult.toExponential(8);
 			const {displayedEquation} = this.state;
 			let equationDisplay = displayedEquation + "=" + resultDisplay;
-			equationDisplay = equationDisplay.slice(equationDisplay.length - 30, equationDisplay.length);
 			this.setState({
 				currVal: currResult,
-				savedVal: 0,
-				base: 0.1,
+				base: 1,
 				currDisplay: resultDisplay,
 				isCompleted: true,
-				isNewNumberEntered: true,
+				isNewNumber: true,
 				isDecimalAdded: false,
 				isOperatorEntered: false,
-				operator: "",
+				operators: [],
 				displayedEquation: equationDisplay
 			});
   	}
 
   	onReset(){
   		this.setState({
-  			isNewNumberEntered: true,
+  			isNewNumber: true,
   			isOperatorEntered: false,
         isDecimalAdded: false,
   			currVal: 0,
-				base: 0.1,
-  			savedVal: 0,
+				base: 1,
 				currDisplay: "0",
-  			operator: "",
+  			operators: [],
         isCompleted: false,
-  			actualEquation: [],
 				displayedEquation: ""
   		});
   	}
